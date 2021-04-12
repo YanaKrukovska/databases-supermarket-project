266566a71361198e739e9fb2b74a4a12b713af8e
YanaKrukovska <jana.krua@gmail.com>
2021-04-12 12:25:05 +0300

    - Added sale service

---
+++ src/main/java/ua/edu/ukma/supermarket/persistence/service/SaleService.java
@@ -0,0 +1,79 @@
+package ua.edu.ukma.supermarket.persistence.service;
+
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+import ua.edu.ukma.supermarket.persistence.model.Response;
+import ua.edu.ukma.supermarket.persistence.model.Sale;
+
+import java.sql.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    private final Connection connection;
+
+    public SaleService(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Response<Sale> createSale(Sale sale) {
+
+        List<String> saleErrors = validateSale(sale);
+        if (!saleErrors.isEmpty()) {
+            return new Response<>(null, saleErrors);
+        }
+
+
+        String query = "INSERT INTO sale (upc, check_number, product_number, selling_price) VALUES (?,?,?,?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, sale.getUpc());
+            statement.setString(2, sale.getReceiptNumber());
+            statement.setInt(3, sale.getProductNumber());
+            statement.setDouble(4, sale.getSellingPrice());
+
+            int rows = statement.executeUpdate();
+
+            if (rows == 0) {
+                return new Response<>(null, Collections.singletonList("Failed to save"));
+            }
+
+            return new Response<>(sale, new LinkedList<>());
+        } catch (SQLException e) {
+            return new Response<>(null, Collections.singletonList(e.getMessage()));
+        }
+    }
+
+    private List<String> validateSale(Sale sale) {
+        List<String> errors = new LinkedList<>();
+
+        if (sale.getUpc() == null || sale.getUpc().isBlank()) {
+            errors.add("UPC can't be empty");
+        }
+        if (sale.getReceiptNumber() == null) {
+            errors.add("Receipt number can't be empty");
+        }
+        if (sale.getProductNumber() < 0) {
+            errors.add("Amount can't be less than 0");
+        }
+        if (sale.getSellingPrice() < 0) {
+            errors.add("Price can't be less than 0");
+        }
+        return errors;
+    }
+
+    @SneakyThrows
+    private Sale extractSale(ResultSet resultSet) {
+        String upc = resultSet.getString("upc");
+        String checkNumber = resultSet.getString("check_number");
+        Integer productNumber = resultSet.getInt("product_number");
+        Double sellingPrice = resultSet.getDouble("selling_price");
+
+        return new Sale(upc, checkNumber, productNumber, sellingPrice);
+    }
+
+}
